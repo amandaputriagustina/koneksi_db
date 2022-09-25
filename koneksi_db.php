a46d516588135c6eb04e7df70ec96612aa96ce09
amandaputriagustina <114270595+amandaputriagustina@users.noreply.github.com>
2022-09-25 12:44:41 +0700

    Add files via upload

---
+++ koneksi_db.php
@@ -0,0 +1,31 @@
+<?php
+
+$host="localhost";
+
+$user="root";
+
+$password="";
+
+$database="koneksi_db";
+
+
+
+$koneksi=mysqli_connect($host,$user,$password);
+
+mysqli_select_db($koneksi,$database);
+
+
+
+//cek koneksi
+
+if($koneksi){
+
+echo "Berhasil Terhubung";
+
+}else{
+
+echo "Gagal Terhubung";
+
+}
+
+?>
